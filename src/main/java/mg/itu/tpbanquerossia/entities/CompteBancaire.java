d6aa8294d3450286e7a4d6429bf93d8f6529e4d8
Rossia <rossia.rasoanomenjanahary@gmail.com>
2022-04-12 12:57:39 +0300

    Etat initial

---
+++ src/main/java/mg/itu/tpbanquerossia/entities/CompteBancaire.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquerossia.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Rossia
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nom;
+    private Integer solde;
+
+    public CompteBancaire() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Integer getSolde() {
+        return solde;
+    }
+
+    public void setSolde(Integer solde) {
+        this.solde = solde;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public CompteBancaire(String nom, int solde) {  
+  this.nom = nom;  
+  this.solde = solde;  
+}  
+                  
+public void deposer(int montant) {  
+  solde += montant;  
+}  
+                  
+public void retirer(int montant) {  
+  if (montant < solde) {  
+    solde -= montant;  
+  } else {
+    solde = 0;
+  }  
+}
+    //il faut l'enlever car on ne doit pas modifier la clé primaire
+    /*public void setId(Long id) {
+        this.id = id;
+    }*/
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mg.itu.tpbanquerossia.entities.CompteBancaire[ id=" + id + " ]";
+    }
+    
+}
